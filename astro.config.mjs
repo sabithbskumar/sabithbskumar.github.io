92b509fc6281f9d341957c6f2c9c7f387a2cf53c
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2026-04-11 00:54:55 +0530

    chore: add SITE_URL env config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const mode = process.env.NODE_ENV ?? "development";
+const env = loadEnv(mode, process.cwd(), "");
+const { SITE_URL } = env;
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sabithbskumar.github.io",
+  site: SITE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
